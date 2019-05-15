3ae6d6dbe5ae023afa8c17680c1e927cbf017b5a
Valeriy <39806983+valerapon@users.noreply.github.com>
2019-05-15 20:24:04 +0600

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-install:
-	clear && mkdir -p bin && g++ -o bin/main main.cpp sources/Algorithm.cpp sources/Lexem.cpp sources/Number.cpp sources/Operator.cpp sources/Variable.cpp sources/Label.cpp sources/Array.cpp sources/Function.cpp sources/Error.cpp
+instal:
+	mkdir -p bin && g++ main.cpp -std=c++17 -lcurl -o bin/main	
--- main.cpp
+++ main.cpp
@@ -1,24 +1,74 @@
-#include "headers/Algorithm.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <curl/curl.h>
+
+bool isRef(std::string &str, int i) { 
+	return str.substr(i, 7) == "http://" || str.substr(i, 8) == "https://";
+} 
+
+bool isRefEnd(char ch) {
+	return  ch == '"' || ch == ')' || ch == ';' || ch == '\'' || ch == '\n' || ch == ' ';
+}
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+	((std::string*)userp)->append((char*)contents, size * nmemb);
+	return size * nmemb;
+}
+
+void findReference(std::vector<std::vector<std::string>> &array, std::string &readBuffer, int DEPTH, int level) {
+	bool isThere;
+	int size = readBuffer.size();
+	std::string ref;
+	for (int k = 0; k < size - 4; ++k) {
+		if (isRef(readBuffer, k)) {
+			k += 4;
+			ref = "http";
+			while (!isRefEnd(readBuffer[k])) {
+				ref += readBuffer[k++];
+			}
+			isThere = false;
+			for (int l = 0; l < DEPTH + 1; ++l) {
+				for (int k = 0; k < array[l].size(); ++k) {
+					if (array[l][k] == ref) {
+						isThere = true;
+						break;
+					}
+				}
+			}
+			if (isThere) {
+				continue;
+			}
+			array[level + 1].push_back(ref);
+			std::cout << ref << std::endl;
+		}
+	}
+}
 
 int main(int argc, char **argv) {
-	try {
-		Algorithm alg;
-		alg.definePoliz();
-		alg.defineLabels();
-		alg.defineConditionsAndLoops();
-		alg.defineFunctions();
-		alg.evaluate_poliz();
-		if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'd') {
-			alg.printPolizTable();
-			alg.printVariableTable();
-			alg.printArrayTable();
-			alg.printLabelTable();
-			alg.printFunctionTable();
+	int DEPTH = std::stoi(argv[1]);
+	bool isThere;
+	std::string ref, readBuffer;
+	std::vector<std::vector<std::string>> array;
+	array.resize(DEPTH + 1);
+	array[0].push_back(argv[2]);
+	CURL *curl;
+  	CURLcode res;
+	for (int i = 0; i < DEPTH; ++i) {
+		for (int j = 0; j < array[i].size(); ++j) {
+  			curl = curl_easy_init();
+    			curl_easy_setopt(curl, CURLOPT_URL, array[i][j].c_str());
+    			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+			res = curl_easy_perform(curl);
+			int size = readBuffer.size();
+			if (size == 0) {
+				continue;
+			}
+			findReference(array, readBuffer, DEPTH, i);
+			curl_easy_cleanup(curl);
 		}
-		alg.deleteData();
-	}
-	catch(...) {
-		error();
 	}
 	return 0;
 }
+
